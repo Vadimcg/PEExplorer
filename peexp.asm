c70a374dad46e979f189fa11fa8abd14e93c504c
Vadimcg <vadim-derevich@yandex.ru>
2017-02-10 06:29:26 +0700

    Added input and output information

--- peexp.asm
+++ peexp.asm
@@ -8,10 +8,16 @@ includelib \masm32\lib\kernel32.lib
 includelib \masm32\lib\masm32.lib
 
 .data
-message db "This is your first assembly program", 0
+titleMessage db "Portable Executable Explorer",13,10, 0
+pathMessage db "Path to your PE file:",13,10, 0
+
+pathBuff db 100 dup(?)
 
 .code 
 main:
-invoke StdOut, addr message
+invoke StdOut, offset titleMessage
+invoke StdOut, offset pathMessage
+invoke StdIn, offset pathBuff, 100
+
 invoke ExitProcess, 0
 end main
