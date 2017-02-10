35578d18ea683fbfa9708fb7961449936e12aa40
Vadimcg <vadim-derevich@yandex.ru>
2017-02-11 06:07:54 +0700

    Start doing reading file

--- peexp.asm
+++ peexp.asm
@@ -8,6 +8,7 @@ includelib \masm32\lib\kernel32.lib
 includelib \masm32\lib\masm32.lib
 
 .data
+consoleTitle BYTE "PEExplorer",0
 titleMessage db "Portable Executable Explorer",13,10, 0
 pathMessage db "Path to your PE file:",13,10, 0
 
@@ -15,9 +16,16 @@ pathBuff db 100 dup(?)
 
 .code 
 main:
+
+invoke SetConsoleTitle, addr consoleTitle
+
 invoke StdOut, offset titleMessage
 invoke StdOut, offset pathMessage
 invoke StdIn, offset pathBuff, 100
+
+
+
+
 
 invoke ExitProcess, 0
 end main
