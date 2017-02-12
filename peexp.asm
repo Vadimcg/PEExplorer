c3d92244fd308e67dfa12ad0e6aefadd67ad148b
Vadimcg <vadim-derevich@yandex.ru>
2017-02-13 06:09:08 +0700

    Added conditon

--- peexp.asm
+++ peexp.asm
@@ -9,8 +9,13 @@ includelib \masm32\lib\masm32.lib
 
 .data
 consoleTitle BYTE "PEExplorer",0
-titleMessage db "Portable Executable Explorer",13,10, 0
-pathMessage db "Path to your PE file:",13,10, 0
+titleMessage BYTE "Portable Executable Explorer",13,10, 0
+pathMessage BYTE "Path to your PE file:",13,10, 0
+
+fileOpenError BYTE  "Can't open file",0
+
+
+fileHandle db ?
 
 pathBuff db 100 dup(?)
 
@@ -23,8 +28,24 @@ invoke StdOut, offset titleMessage
 invoke StdOut, offset pathMessage
 invoke StdIn, offset pathBuff, 100
 
+invoke 
+    CreateFile,
+    GENERIC_READ,
+    DO_NOT_SHARE,
+    NULL,
+    OPEN_EXISTING,
+    FILE_ATTRIBUTE_NORMAL,
+    0
+
+mov fileHandle,eax
+
+cmp fileHandle,INVALID_HANDLE_VALUE
+jz fileOpenError
 
 
+
+fileOpenError:
+invoke StdOut,fileOpenError
 
 
 invoke ExitProcess, 0
