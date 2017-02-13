55524d01dad8d6791cd2694d5ba5ccd578a7063a
Vadimcg <vadim-derevich@yandex.ru>
2017-02-13 11:09:39 +0700

    Fixed some bugs

--- peexp.asm
+++ peexp.asm
@@ -6,16 +6,18 @@ include \masm32\include\kernel32.inc
 include \masm32\include\masm32.inc
 includelib \masm32\lib\kernel32.lib
 includelib \masm32\lib\masm32.lib
+include \masm32\include\user32.inc 
+includelib \masm32\lib\user32.lib
 
 .data
 consoleTitle BYTE "PEExplorer",0
 titleMessage BYTE "Portable Executable Explorer",13,10, 0
 pathMessage BYTE "Path to your PE file:",13,10, 0
 
-fileOpenError BYTE  "Can't open file",0
+fileOpenErrorMessege BYTE  "Can't open file",0
 
-
-fileHandle db ?
+FileName db "C:\Users\Vadimcg\Desktop\MASMProjects\test.txt",NULL
+fileHandle HANDLE  ?
 
 pathBuff db 100 dup(?)
 
@@ -28,14 +30,7 @@ invoke StdOut, offset titleMessage
 invoke StdOut, offset pathMessage
 invoke StdIn, offset pathBuff, 100
 
-invoke 
-    CreateFile,
-    GENERIC_READ,
-    DO_NOT_SHARE,
-    NULL,
-    OPEN_EXISTING,
-    FILE_ATTRIBUTE_NORMAL,
-    0
+invoke  CreateFile,FileName, GENERIC_READ, FILE_SHARE_READ OR FILE_SHARE_WRITE,NULL,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,NULL
 
 mov fileHandle,eax
 
@@ -43,9 +38,8 @@ cmp fileHandle,INVALID_HANDLE_VALUE
 jz fileOpenError
 
 
-
 fileOpenError:
-invoke StdOut,fileOpenError
+invoke StdOut,fileOpenErrorMessege
 
 
 invoke ExitProcess, 0
