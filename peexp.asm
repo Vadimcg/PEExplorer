eec78d9ff5285cf7528a075e4d78a43b2eab5caa
Vadimcg <vadim-derevich@yandex.ru>
2017-02-17 06:58:37 +0700

    made reading of IMAGE_DOS_SIGNATURE

--- peexp.asm
+++ peexp.asm
@@ -11,28 +11,36 @@ includelib \masm32\lib\user32.lib
 
  
 .data
+
 consoleTitle BYTE "PEExplorer",0
 titleMessage BYTE "Portable Executable Explorer",13,10, 0
 pathMessage BYTE "Path to your PE file:",13,10, 0
 
 fileOpenSuccessMessege BYTE  "File was opended!",13,10,0
+errorWhileReadingMessege BYTE  "Error while reading",13,10,0
 fileOpenErrorMessege BYTE  "Can't open file",0
 
 FileName db "C:\Users\Vadimcg\Desktop\MASMProjects\test.txt",NULL
 fileHandle HANDLE  ?
 
-pathBuff db 100 dup(?)
+testValue db 9
+
+buff db 100 dup(?)
+;Variable will store amout of read bytes
+readInfo WORD 0
 
 .code 
 main:
+
+mov eax,offset testValue
 
 invoke SetConsoleTitle, addr consoleTitle
 
 invoke StdOut, offset titleMessage
 invoke StdOut, offset pathMessage
-invoke StdIn, offset pathBuff, 100
+invoke StdIn, offset buff, 100
 
-invoke  CreateFile,addr pathBuff, GENERIC_READ, FILE_SHARE_READ OR FILE_SHARE_WRITE,NULL,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,NULL
+invoke  CreateFile,addr FileName, GENERIC_READ, FILE_SHARE_READ OR FILE_SHARE_WRITE,NULL,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,NULL
 
 mov fileHandle,eax
 cmp fileHandle,INVALID_HANDLE_VALUE
@@ -40,10 +48,24 @@ jz fileOpenError
 
 
 invoke StdOut,offset fileOpenSuccessMessege
+
+;Read IMAGE_DOS_SIGNATURE it must be "MZ" for exe files OFFSET 0  SIZE WORD
+invoke ReadFile,addr fileHandle,addr buff,2,readInfo,0
+
+cmp readInfo,2
+jnz fileOpenError
+
+
+
 jmp endPE
+
+readingError:
+invoke StdOut,offset errorWhileReadingMessege
 
 fileOpenError:
 invoke StdOut,offset fileOpenErrorMessege
+
+
 
 endPE:
 invoke ExitProcess, 0
