0ae9a6ecf2b715ee2be856a6ddfe84ef68086b53
Vadimcg <vadim-derevich@yandex.ru>
2017-02-21 07:07:06 +0700

    Added some strings, commints, changed order of functions

--- peexp.asm
+++ peexp.asm
@@ -21,12 +21,15 @@ errorWhileReadingMessege BYTE  "Error while reading",13,10,0
 fileOpenErrorMessege BYTE  "Can't open file",0
 fileSizeErrorMessege BYTE  "Can't determinate file size",0
 
+fileSizeIsMessege BYTE  "File size is:",0
+bytesMessege BYTE " bytes",0
+
 FileName db "C:\Users\Vadimcg\Desktop\MASMProjects\ff.exe",NULL
 fileHandle DWORD  ?
 
 buff db 100 dup(?)
 ;Variable will store amout of read bytes
-readInfo dd 0
+readInfo dd ?
 
 .code 
 main:
@@ -41,19 +44,30 @@ xor eax,eax
 ;Try to open file
 invoke  CreateFile,addr FileName, GENERIC_READ,FILE_SHARE_DELETE,NULL,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,0
 ;Cheecking resault
-mov fileHandle,eax
-
-.IF fileHandle==INVALID_HANDLE_VALUE
-    jmp fileOpenError
-.ENDIF
+mov fileHandle,eax    
 
 cmp fileHandle,INVALID_HANDLE_VALUE
-
+jz fileOpenError
 
 ;File was opend successfully ! -messege
 invoke StdOut,offset fileOpenSuccessMessege
 
+;--------------------------------GETTING SIZE------------------------------
+invoke GetFileSize,fileHandle, offset readInfo
+
+;mov readInfo,eax
+cmp readInfo,INVALID_FILE_SIZE
+jz fileSizeError
+
+;Show information about size
+invoke StdOut, offset fileSizeIsMessege
+invoke StdOut, offset readInfo
+invoke StdOut, offset bytesMessege
+;-------------------------------END----------------------------------------
+
+
 ;---------------------------------READING----------------------------------
+mov readInfo,0
 ;Read IMAGE_DOS_SIGNATURE it must be "MZ" for exe files OFFSET 0  SIZE WORD
 invoke ReadFile,fileHandle,addr buff,2,addr readInfo,0
 cmp readInfo,2
@@ -62,25 +76,10 @@ cmp readInfo,2
 invoke StdOut,offset buff
 
 jnz readingError
-
-jmp closeFile
 ;---------------------------------END READING------------------------------
 
 
-;Get file size
-invoke GetFileSize, offset fileHandle, NULL
-mov readInfo,eax
-cmp readInfo,INVALID_FILE_SIZE
-jz fileSizeError
-
-invoke StdOut, offset readInfo
-
-jmp endPE
-
-
-
-jmp endPE
-
+jmp closeFile
 
 fileOpenError:
 invoke StdOut,offset fileOpenErrorMessege
