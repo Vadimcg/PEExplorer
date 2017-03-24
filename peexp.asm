aac6ee1532eb0d8225a7c616fc7ca00eea8abcc7
Vadimcg <vadim-derevich@yandex.ru>
2017-03-25 06:54:06 +0800

    Added 'file aligment'

--- peexp.asm
+++ peexp.asm
@@ -490,6 +490,22 @@ invoke StdOut, offset buff
 
 invoke StdOut,offset nLine
 
+;SectionAlignment
+mov eax,adreessVal
+add eax,4
+mov adreessVal,eax
+
+invoke StdOut, offset fileAlignment
+
+invoke SetFilePointer,fileHandle,adreessVal,0,FILE_BEGIN
+invoke ReadFile,fileHandle,offset buff,4,addr readInfo,0
+
+mov eax,DWORD PTR buff
+invoke dwtoa,eax,offset buff
+invoke StdOut, offset buff
+
+invoke StdOut,offset nLine
+
 ;---------------------------------END READING------------------------------
 
 
