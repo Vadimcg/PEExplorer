eea8b2edba46a97761af9fd1c51deba2a45cf14d
Vadimcg <vadim-derevich@yandex.ru>
2017-03-28 13:22:00 +0800

    made showing 'MinorOperatingSystemVersion'

--- peexp.asm
+++ peexp.asm
@@ -524,6 +524,24 @@ invoke StdOut, offset buff
 invoke StdOut,offset nLine
 
 
+;MinorOperatingSystemVersion
+mov eax,adreessVal
+add eax,2
+mov adreessVal,eax
+
+invoke StdOut, offset minorOperatingSystemVersion
+
+invoke SetFilePointer,fileHandle,adreessVal,0,FILE_BEGIN
+invoke ReadFile,fileHandle,offset buff,2,addr readInfo,0
+
+xor ax,ax
+mov ax,WORD PTR buff
+invoke dwtoa,eax,offset buff
+invoke StdOut, offset buff
+
+invoke StdOut,offset nLine
+
+
 ;---------------------------------END READING------------------------------
 
 
